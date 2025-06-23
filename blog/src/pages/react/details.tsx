01db6f18c4a9029e4680d21126538caac3d83b5c
doheejung <dohxxzun@mobigen.com>
2025-06-23 13:21:32 +0900

    feat: 게시글 상세 내용 하위 컴포넌트에서 조회하도록 수정

--- blog/src/pages/react/details.tsx
+++ blog/src/pages/react/details.tsx
@@ -3,26 +3,21 @@ import { Helmet } from 'react-helmet-async';
 import { useParams } from 'src/routes/hooks';
 
 import { CONFIG } from 'src/config-global';
-import { useGetPost } from 'src/actions/blog';
 
 import { PostDetailsView } from 'src/sections/blog/view';
 
 // ----------------------------------------------------------------------
 
-const metadata = { title: `Post details | Dashboard - ${CONFIG.appName}` };
-
 export default function Page() {
-  const { title = '' } = useParams();
-
-  const { post, postLoading, postError } = useGetPost(title);
+  const { title } = useParams();
 
   return (
     <>
       <Helmet>
-        <title> {metadata.title}</title>
+        <title> {`${title ?? ''} | ${CONFIG.appName}`}</title>
       </Helmet>
 
-      <PostDetailsView post={post} loading={postLoading} error={postError} />
+      <PostDetailsView title={title ?? ''} category="react" />
     </>
   );
 }
